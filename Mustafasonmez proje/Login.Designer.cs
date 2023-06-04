09007cd4a9821dcc47c44228d7a3204871b89484
denicoder@gmail.com <denicoder@gmail.com>
2023-06-05 02:21:30 +0300

    tasarım iyileştirmeleri

--- Mustafasonmez proje/Login.Designer.cs
+++ Mustafasonmez proje/Login.Designer.cs
@@ -28,105 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.label1 = new System.Windows.Forms.Label();
             this.KullaniciAdiTb = new System.Windows.Forms.TextBox();
             this.SifreTb = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(63, 109);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(234, 191);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Averta-Bold", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(526, 51);
+            this.label1.Font = new System.Drawing.Font("Averta-Bold", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(479, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 57);
+            this.label1.Size = new System.Drawing.Size(248, 62);
             this.label1.TabIndex = 1;
-            this.label1.Text = "GIRIS";
+            this.label1.Text = "GİRİŞ YAP";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // KullaniciAdiTb
             // 
-            this.KullaniciAdiTb.Location = new System.Drawing.Point(536, 166);
+            this.KullaniciAdiTb.BackColor = System.Drawing.SystemColors.Info;
+            this.KullaniciAdiTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.KullaniciAdiTb.Font = new System.Drawing.Font("Averta-Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.KullaniciAdiTb.ForeColor = System.Drawing.Color.Crimson;
+            this.KullaniciAdiTb.Location = new System.Drawing.Point(557, 127);
+            this.KullaniciAdiTb.Multiline = true;
             this.KullaniciAdiTb.Name = "KullaniciAdiTb";
-            this.KullaniciAdiTb.Size = new System.Drawing.Size(129, 22);
+            this.KullaniciAdiTb.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.KullaniciAdiTb.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.KullaniciAdiTb.Size = new System.Drawing.Size(169, 35);
             this.KullaniciAdiTb.TabIndex = 2;
             // 
             // SifreTb
             // 
-            this.SifreTb.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.SifreTb.Location = new System.Drawing.Point(536, 218);
+            this.SifreTb.BackColor = System.Drawing.SystemColors.Info;
+            this.SifreTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.SifreTb.Font = new System.Drawing.Font("Averta-Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.SifreTb.ForeColor = System.Drawing.Color.Crimson;
+            this.SifreTb.Location = new System.Drawing.Point(557, 180);
+            this.SifreTb.Multiline = true;
             this.SifreTb.Name = "SifreTb";
-            this.SifreTb.Size = new System.Drawing.Size(129, 24);
+            this.SifreTb.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.SifreTb.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.SifreTb.Size = new System.Drawing.Size(169, 35);
             this.SifreTb.TabIndex = 3;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Averta-Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(385, 158);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(145, 30);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Kullanıcı adı";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Averta-Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(446, 212);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(84, 30);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Parola";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(464, 267);
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Averta-Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(548, 246);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(146, 42);
+            this.button1.Size = new System.Drawing.Size(178, 42);
             this.button1.TabIndex = 7;
             this.button1.Text = "GİRİŞ YAP";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // pictureBox1
             // 
-            this.button2.Location = new System.Drawing.Point(636, 267);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(146, 42);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "ŞİFRE SIFIRLA";
-            this.button2.UseVisualStyleBackColor = true;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(50, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(315, 260);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Averta-Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(423, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 26);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Kullanıcı Adı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Averta-Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(479, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 26);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Parola";
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1201, 458);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label4);
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(801, 320);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.SifreTb);
             this.Controls.Add(this.KullaniciAdiTb);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.Color.Crimson;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Login";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -136,14 +148,12 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox KullaniciAdiTb;
         private System.Windows.Forms.TextBox SifreTb;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Mustafasonmez proje/Uyeler.Designer.cs
+++ Mustafasonmez proje/Uyeler.Designer.cs
@@ -39,38 +39,46 @@
             // 
             // uyedatagrid
             // 
+            this.uyedatagrid.BackgroundColor = System.Drawing.SystemColors.Info;
             this.uyedatagrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.uyedatagrid.Location = new System.Drawing.Point(423, 2);
+            this.uyedatagrid.GridColor = System.Drawing.Color.Crimson;
+            this.uyedatagrid.Location = new System.Drawing.Point(-17, 180);
             this.uyedatagrid.Name = "uyedatagrid";
             this.uyedatagrid.RowHeadersWidth = 51;
             this.uyedatagrid.RowTemplate.Height = 24;
-            this.uyedatagrid.Size = new System.Drawing.Size(694, 490);
+            this.uyedatagrid.Size = new System.Drawing.Size(1181, 380);
             this.uyedatagrid.TabIndex = 0;
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Crimson;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(255, 140);
+            this.button1.Font = new System.Drawing.Font("Averta-Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Info;
+            this.button1.Location = new System.Drawing.Point(702, 89);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(149, 34);
+            this.button1.Size = new System.Drawing.Size(176, 45);
             this.button1.TabIndex = 2;
             this.button1.Text = "ARA";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // AraUyeTextbox
             // 
-            this.AraUyeTextbox.Location = new System.Drawing.Point(12, 140);
+            this.AraUyeTextbox.BackColor = System.Drawing.SystemColors.Info;
+            this.AraUyeTextbox.ForeColor = System.Drawing.Color.Crimson;
+            this.AraUyeTextbox.Location = new System.Drawing.Point(218, 91);
             this.AraUyeTextbox.Multiline = true;
             this.AraUyeTextbox.Name = "AraUyeTextbox";
-            this.AraUyeTextbox.Size = new System.Drawing.Size(234, 34);
+            this.AraUyeTextbox.Size = new System.Drawing.Size(280, 45);
             this.AraUyeTextbox.TabIndex = 3;
+            this.AraUyeTextbox.Text = "İsim giriniz";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Averta-Bold", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(42, 21);
+            this.label1.Location = new System.Drawing.Point(365, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(271, 57);
             this.label1.TabIndex = 4;
@@ -78,31 +86,37 @@
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.Crimson;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(139, 286);
+            this.button2.Font = new System.Drawing.Font("Averta-Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Info;
+            this.button2.Location = new System.Drawing.Point(520, 89);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(149, 34);
+            this.button2.Size = new System.Drawing.Size(176, 45);
             this.button2.TabIndex = 5;
             this.button2.Text = "GERİ";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Yeniletextbox
             // 
+            this.Yeniletextbox.BackColor = System.Drawing.Color.Crimson;
             this.Yeniletextbox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Yeniletextbox.Location = new System.Drawing.Point(139, 231);
+            this.Yeniletextbox.Font = new System.Drawing.Font("Averta-Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Yeniletextbox.ForeColor = System.Drawing.SystemColors.Info;
+            this.Yeniletextbox.Location = new System.Drawing.Point(884, 89);
             this.Yeniletextbox.Name = "Yeniletextbox";
-            this.Yeniletextbox.Size = new System.Drawing.Size(149, 34);
+            this.Yeniletextbox.Size = new System.Drawing.Size(176, 46);
             this.Yeniletextbox.TabIndex = 6;
             this.Yeniletextbox.Text = "YENİLE";
-            this.Yeniletextbox.UseVisualStyleBackColor = true;
+            this.Yeniletextbox.UseVisualStyleBackColor = false;
             this.Yeniletextbox.Click += new System.EventHandler(this.Yeniletextbox_Click);
             // 
             // Uyeler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1121, 490);
+            this.ClientSize = new System.Drawing.Size(1160, 560);
             this.Controls.Add(this.Yeniletextbox);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
